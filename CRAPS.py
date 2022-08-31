387b4070970005ba2194a44010e0bfe825f08548
Gabriel Santos Bueno <gasantosbueno@gmail.com>
2022-08-31 20:34:04 -0300

    Update CRAPS.py

--- CRAPS.py
+++ CRAPS.py
@@ -1,3 +1,11 @@
+'''CRAPS – Dois dados são lançados. Então soma-se as faces:
+* Se a soma for 7 ou 11 na primeira rodada, o jogador ganha.
+* Se for 2, 3 ou 12 na primeira rodada o jogador perde.
+* Se a soma for 4, 5, 6, 8, 9 ou 10 na primeira rodada. Esta soma se torna o ponto do jogador.
+* Para ganhar, o jogador continua jogando o dado até a soma ser igual a seu ponto. 
+* O jogador perde se a soma for igual a 7.
+'''
+
 import random
 
 gameover = False
