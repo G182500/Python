a56f2a24591e64f0ee3b288ccdd538bb49413870
Gabriel Santos Bueno <gasantosbueno@gmail.com>
2024-05-02 08:55:12 -0300

    Update bd.py

--- CompararPDFPlanilha/bd.py
+++ CompararPDFPlanilha/bd.py
@@ -3,13 +3,12 @@ import uuid
 from datetime import datetime
 
 def salvarBanco(nome_pdf, values, status):
-    cnx = mysql.connector.connect(user='b09a83c4724f0a', 
-                                    password='5968583e',
-                                    host='us-cdbr-east-06.cleardb.net', 
-                                    database='heroku_2409cf2cf4dd08d', 
+    cnx = mysql.connector.connect(user='xxxxxx', 
+                                    password='yyyyyyy',
+                                    host='sdsdsdsdsds', 
+                                    database='blablabla', 
                                     charset="utf8")
-    #cnx = mysql.connector.connect(user='root', password='synvia@123',host='localhost', database='localrobogq')
-
+    
     try:
         cursor = cnx.cursor()
 
